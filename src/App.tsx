2a7d0fbaa2df4622180bd7e90677f8f9ba8f03de
Kenny-136 <103553256+Kenny-136@users.noreply.github.com>
2023-09-29 19:34:43 +1000

    routers template

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Home/Home";
+import NotFound from "./components/NotFound/NotFound";
+import Employee from "./components/Employee/Employee";
 function App() {
-	return <></>;
+	return (
+		<>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="*" element={<NotFound />} />
+					<Route path="employee/:id" element={<Employee />} />
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
 }
 
 export default App;
